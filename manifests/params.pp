a04df4a947ed169abc7bf232174bd289e2b1a99c
Artur Faizullin <artur@thehyve.nl>
2018-10-15 17:52:55 +0200

    add parameters checks

---
+++ manifests/params.pp
@@ -0,0 +1,19 @@
+class keycloak-docker::params (
+  String $db_password = lookup('keycloak-docker::db_password', String, first, ''),
+  String $keycloak_user == lookup('keycloak-docker::keycloak_user', String, first, ''),
+  String $keycloak_password == lookup('keycloak-docker::keycloak_password', String, first, ''),
+  String $keycloak_fqdn == lookup('keycloak-docker::keycloak_fqdn', String, first, ''),
+) {
+  if $db_password == '' {
+    fail('No database password specified. Please configure keycloak-docker::db_password')
+  }
+  if $keycloak_user == '' {
+    fail('No admin user specified. Please configure keycloak-docker::keycloak_user')
+  }
+  if $keycloak_password == '' {
+    fail('No admin password specified. Please configure keycloak-docker::keycloak_password')
+  }
+  if $keycloak_fqdn == '' {
+    fail('No server fqdn specified. Please configure keycloak-docker::keycloak_fqdn')
+  }
+}
