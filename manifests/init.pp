54c95c4b6f305cd41cb46d848705bcaf3e2c92a6
Artur Faizullin <artur@thehyve.nl>
2018-10-16 17:30:03 +0200

    add docker to init file

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,4 +1,8 @@
 class keycloak_docker inherits keycloak_docker::params {
+  include 'docker'
+  class { 'docker::compose':
+    ensure => present,
+  }
   file { $docker_compose_file_path:
     ensure => file,
     content => template('keycloak_docker/docker-compose.yml.erb'),
