e1af9583bb63f51eeb8b4dafbf71e5317330e500
Artur Faizullin <artur@thehyve.nl>
2018-10-16 17:05:56 +0200

    fix remove extra = character

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,8 +1,8 @@
-class keycloak_docker::params (
+ctransmart_packerlass keycloak_docker::params (
   String $db_password = lookup('keycloak_docker::db_password', String, first, ''),
-  String $keycloak_user == lookup('keycloak_docker::keycloak_user', String, first, ''),
-  String $keycloak_password == lookup('keycloak_docker::keycloak_password', String, first, ''),
-  String $keycloak_fqdn == lookup('keycloak_docker::keycloak_fqdn', String, first, ''),
+  String $keycloak_user = lookup('keycloak_docker::keycloak_user', String, first, ''),
+  String $keycloak_password = lookup('keycloak_docker::keycloak_password', String, first, ''),
+  String $keycloak_fqdn = lookup('keycloak_docker::keycloak_fqdn', String, first, ''),
   String $docker_compose_file_path = lookup('keycloak_docker::docker_compose_file_path', String, first, '/tmp/docker-compose.yml'),
 ) {
   if $db_password == '' {
