71215da6b13c71cacc161db3ab09507798e06274
Artur Faizullin <artur@thehyve.nl>
2018-10-25 12:10:11 +0200

    change behavior of docker_compose_file_path variable, now it is used for docker_compose naming and as a part of docker-compose file path

--- manifests/init.pp
+++ manifests/init.pp
@@ -3,11 +3,11 @@ class keycloak_docker inherits keycloak_docker::params {
   class { 'docker::compose':
     ensure => present,
   }
-  file { $docker_compose_file_path:
+  file { /${docker_compose_file_path}/docker-compose.yml:
     ensure => file,
     content => template('keycloak_docker/docker-compose.yml.erb'),
-  } ~> docker_compose { 'keycloak_compose':
-    compose_files => [ $docker_compose_file_path ],
+  } ~> docker_compose { "$docker_compose_file_path":
+    compose_files => [ /${docker_compose_file_path}/docker-compose.yml ],
     ensure => present,
   }
 }
--- manifests/params.pp
+++ manifests/params.pp
@@ -3,7 +3,7 @@ class keycloak_docker::params (
   String $keycloak_user = lookup('keycloak_docker::keycloak_user', String, first, ''),
   String $keycloak_password = lookup('keycloak_docker::keycloak_password', String, first, ''),
   String $keycloak_fqdn = lookup('keycloak_docker::keycloak_fqdn', String, first, ''),
-  String $docker_compose_file_path = lookup('keycloak_docker::docker_compose_file_path', String, first, '/tmp/docker-compose.yml'),
+  String $docker_compose_file_path = lookup('keycloak_docker::docker_compose_file_path', String, first, 'tmp'),
   String $keycloak_version = lookup('keycloak_docker::keycloak_version', String, first, 'latest'),
   String $postgresql_version = lookup('keycloak_docker::postgresql_version', String, first, 'latest'),
 ) {
