22e8ad019b2136ba23e079d656b3330458f4b3ee
Artur Faizullin <artur@thehyve.nl>
2018-10-25 12:36:31 +0200

    fix missing qoutes

--- manifests/init.pp
+++ manifests/init.pp
@@ -3,7 +3,7 @@ class keycloak_docker inherits keycloak_docker::params {
   class { 'docker::compose':
     ensure => present,
   }
-  file { /${docker_compose_file_path}/docker-compose.yml:
+  file { "/${docker_compose_file_path}/docker-compose.yml":
     ensure => file,
     content => template('keycloak_docker/docker-compose.yml.erb'),
   } ~> docker_compose { "$docker_compose_file_path":
